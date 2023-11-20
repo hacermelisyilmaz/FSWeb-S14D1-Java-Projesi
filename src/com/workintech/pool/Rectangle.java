56a5b909752d277ab95c071e93563caa8f48a4e6
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-20 13:23:27 +0300

    Rectangle constructor

--- src/com/workintech/pool/Rectangle.java
+++ src/com/workintech/pool/Rectangle.java
@@ -1,5 +1,14 @@
 package com.workintech.pool;
 
 public class Rectangle {
+    private double width;
+    private double length;
 
+    public Rectangle(double width, double length) {
+        if (width < 0) this.width = 0;
+        else this.width = width;
+
+        if (length < 0) this.length = 0;
+        else this.length = length;
+    }
 }
