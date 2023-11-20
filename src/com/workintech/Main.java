0850d6ee28def8d188f937c437b2d85bea4399ca
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-20 13:35:58 +0300

    Print lines for pool

--- src/com/workintech/Main.java
+++ src/com/workintech/Main.java
@@ -2,18 +2,33 @@ package com.workintech;
 
 import com.workintech.cylinder.Circle;
 import com.workintech.cylinder.Cylinder;
+import com.workintech.pool.Cuboid;
+import com.workintech.pool.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("*** CYLINDER ***");
         Circle circle = new Circle(10);
-        System.out.println("com.workintech.cylinder.Circle radius: " + circle.getRadius());
-        System.out.println("com.workintech.cylinder.Circle area: " + circle.getArea());
+        System.out.println("Circle radius: " + circle.getRadius());
+        System.out.println("Circle area: " + circle.getArea());
 
         Cylinder cylinder = new Cylinder(20, 5);
-        System.out.println("com.workintech.cylinder.Cylinder radius: " + cylinder.getRadius());
-        System.out.println("com.workintech.cylinder.Cylinder height: " + cylinder.getHeight());
-        System.out.println("com.workintech.cylinder.Cylinder circle area: " + cylinder.getArea());
-        System.out.println("com.workintech.cylinder.Cylinder volume: " + cylinder.getVolume());
+        System.out.println("Cylinder radius: " + cylinder.getRadius());
+        System.out.println("Cylinder height: " + cylinder.getHeight());
+        System.out.println("Cylinder circle area: " + cylinder.getArea());
+        System.out.println("Cylinder volume: " + cylinder.getVolume());
+
+        System.out.println("*** POOL ***");
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("Rectangle width: " + rectangle.getWidth());
+        System.out.println("Rectangle length: " + rectangle.getLength());
+        System.out.println("Rectangle area: " + rectangle.getArea());
+
+        Cuboid cuboid = new Cuboid(10, 20, 5);
+        System.out.println("Cuboid width: " + cuboid.getWidth());
+        System.out.println("Cuboid length: " + cuboid.getLength());
+        System.out.println("Cuboid height: " + cuboid.getHeight());
+        System.out.println("Cuboid rectangle area: " + cuboid.getArea());
+        System.out.println("Cuboid volume: " + cuboid.getVolume());
     }
 }
