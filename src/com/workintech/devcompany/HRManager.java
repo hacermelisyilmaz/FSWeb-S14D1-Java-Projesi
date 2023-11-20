7f42641882024cf4c81bc71a63c6013c50d7a5a3
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-20 15:10:55 +0300

    Employee child classes created. work() override for HRManager

---
+++ src/com/workintech/devcompany/HRManager.java
@@ -0,0 +1,8 @@
+package com.workintech.devcompany;
+
+public class HRManager extends Employee {
+    @Override
+    public String work() {
+        return "HR Manager has started to work.";
+    }
+}
---
+++ src/com/workintech/devcompany/SeniorDeveloper.java
@@ -0,0 +1,4 @@
+package com.workintech.devcompany;
+
+public class SeniorDeveloper {
+}
